1df4429edd593d7ae421a441225ef8c21cadcca6
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-03-20 18:44:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 function checkNumber(num) {
   //your code here
-	if(num % 2 == 0 && )
+	if(num % 2 == 0 && num % 5 == 0 ){
+		return true;
+	}
+	return false;
 	
 }
 
-// const num = parseInt(prompt("Enter a number."));
+const num = parseInt(prompt("Enter a number."));
 alert(checkNumber(num));
